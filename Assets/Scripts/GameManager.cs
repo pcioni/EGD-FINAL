916cc2b0817ecfab0f4ab929705b97461d88d67e
Philip Cioni <philcioni@gmail.com>
2016-10-01 15:39:27 -0400

    Basic managers. Basic main menu.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+
+    void Awake() {
+        if (instance == null)
+            instance = this;
+
+        //Enforce singleton pattern.
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Loader : MonoBehaviour {
+
+    public GameObject gameManager;
+
+    void Awake() {
+        //Check if a GameManager has already been assigned to static variable GameManager.instance or if it's still null
+        if (GameManager.instance == null) {
+            Instantiate(gameManager);
+        }
+    }
+}
