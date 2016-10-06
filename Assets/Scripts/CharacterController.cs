4eec43100b300faee1849712496098b353b96131
Philip Cioni <philcioni@gmail.com>
2016-10-06 17:17:29 -0400

    Basic horizontal movement

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+
+    public float maxSpeed = 10f;
+    private Rigidbody2D rb;
+
+    void Awake() {
+    }
+
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+    void FixedUpdate() {
+        HandleMovement();
+    }
+
+    void HandleMovement() {
+        //InputManager is currently set to 1/10 of a second to smooth input from +-1 to 0
+        float dx = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(dx * maxSpeed, rb.velocity.y);
+    }
+
+	void Update () {
+	
+	}
+}
