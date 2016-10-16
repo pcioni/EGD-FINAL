5743e032fbb5f4dcab3b2623ec0fc15f4876b8d6
Daniel Haynes <hayned2@gmail.com>
2016-10-15 20:32:44 -0400

    Made a lot of improvements to the battle framework.

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -9,8 +9,13 @@ public class BattleManager : MonoBehaviour {
 	List<GameObject> participants;
 	string state;
 	int picker;
+	int picker2;
 	bool awaiting_input;
 	bool victory;
+	bool continuer;
+	bool await_result;
+	char need_target;
+	string pending_message;
 
 	// Use this for initialization
 	void Start () {
@@ -19,69 +24,172 @@ public class BattleManager : MonoBehaviour {
 		bad_guys = new List<GameObject> ();
 		participants = new List<GameObject> ();
 		StartBattle (new List<string>{ "Red", "Green", "Blue" }, new List<string>{ "Blue", "Blue", "Red", "Red" });
-		awaiting_input = true;
+		awaiting_input = false;
 		victory = false;
+		continuer = false;
+		await_result = false;
+		need_target = 'n';
+	}
+
+	public void SendMessagey(string message){
+		Debug.Log (message);
+		awaiting_input = true;
+	}
+
+	public void NeedTargeting(char which){
+		need_target = which;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (awaiting_input) {
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				awaiting_input = false;
+			}
+			return;
+		}
 		switch (state) {
 
 		case ("not started"):
 			return;
 
 		case ("instantiated"):
-			Debug.Log ("A battle has begun!");
-			picker = 0;
+			SendMessagey("A battle has begun!");
+			picker = -1;
 			state = "pick actions";
-			Debug.Log ("Pick an action for " + good_guys [picker].name + ": A to attack or G to guard!");
 			return;
 
 		case ("pick actions"):
-			if (Input.GetKeyDown (KeyCode.A) || Input.GetKeyDown (KeyCode.G) || Input.GetKeyDown(KeyCode.V)) {
-				if (Input.GetKeyDown (KeyCode.A)) {
-					Debug.Log (good_guys [picker].name + " will attack this turn!");
-					good_guys [picker].GetComponent<FightBehavior> ().setAction ("attacks");
-				} else if (Input.GetKeyDown (KeyCode.G)) {
-					Debug.Log (good_guys [picker].name + " will guard this turn!");
-					good_guys [picker].GetComponent<FightBehavior> ().setAction ("guards themself");
-				} else if (Input.GetKeyDown (KeyCode.V)) {
-					Debug.Log (good_guys [picker].name + " has invoked the win condition!");
-					good_guys [picker].GetComponent<FightBehavior> ().setAction ("defeats all the enemies");
+			if (picker == -1) {
+				picker++;
+				Debug.Log ("Pick an action for " + good_guys [picker].name + ": A to attack or G to guard!");
+			}
+			if (Input.GetKeyDown (KeyCode.Backspace)) {
+				if (picker > 0) {
+					picker--;
+					Debug.Log ("Pick an action for " + good_guys [picker].name + ": A to attack or G to guard!");
 				}
+			}
+			if (Input.GetKeyDown (KeyCode.A) || Input.GetKeyDown (KeyCode.G) || Input.GetKeyDown(KeyCode.V)) {
+				continuer = true;
+				if (Input.GetKeyDown (KeyCode.A)) {
+					Debug.Log (good_guys [picker].GetComponent<FightBehavior> ().setAction ("attacks"));
+				} else if (Input.GetKeyDown (KeyCode.G)) {
+					Debug.Log (good_guys [picker].GetComponent<FightBehavior> ().setAction ("guards"));
+				} else if (Input.GetKeyDown (KeyCode.V)) {
+					SendMessagey (good_guys [picker].GetComponent<FightBehavior> ().setAction ("insta-kill"));
+				}
+				if (need_target == 'e') {
+					state = "select enemy";
+					picker2 = -1;
+					return;
+				} else if (need_target == 'a') {
+					picker2 = -1;
+					state = "select teammate";
+					return;
+				}
+			}
+			if (continuer && !awaiting_input) {
+				continuer = false;
 				picker++;
 				if (picker >= good_guys.Count) {
-					Debug.Log ("All actions have been selected! Press space to fight!");
+					SendMessagey ("All actions have been selected! Press space to fight!");
 					state = "commence";
 					picker = 0;
-					awaiting_input = true;
 					return;
 				}
 				Debug.Log ("Pick an action for " + good_guys [picker].name + ": A to attack or G to guard!");
 			}
 			return;
 
-		case ("commence"):
-			if (Input.GetKeyDown (KeyCode.Space)) {
-				awaiting_input = false;
+		case ("select enemy"):
+			if (Input.GetKeyDown (KeyCode.Backspace)) {
+				state = "pick actions";
+				continuer = false;
+				Debug.Log ("Pick an action for " + good_guys [picker].name + ": A to attack or G to guard!");
 			}
-			if (!awaiting_input) {
-				if (picker < participants.Count) {
-					participants [picker].GetComponent<FightBehavior> ().doAction ();
-					if (victory) {
-						picker = participants.Count;
+			if (picker2 == -1) {
+				picker2++;
+				Debug.Log ("Who will " + good_guys[picker].name + " target? Currently targeting: " + bad_guys [picker2].name);
+			} else {
+				if (Input.GetKeyDown (KeyCode.W)) {
+					picker2--;
+					if (picker2 < 0) {
+						picker2 = bad_guys.Count - 1;
 					}
-					picker++;
-					awaiting_input = true;
-				} 
-				else if (picker == participants.Count) {
-					picker++;
-				} 
-				else {
-					picker = 0;
-					state = "check win";
-					Debug.Log ("The turn has ended. Press space to continue.");
+					Debug.Log ("Who will " + good_guys[picker].name + " target? Currently targeting: " + bad_guys [picker2].name);
+				} else if (Input.GetKeyDown (KeyCode.S)) {
+					picker2++;
+					if (picker2 >= bad_guys.Count) {
+						picker2 = 0;
+					}
+					Debug.Log ("Who will " + good_guys[picker].name + " target? Currently targeting: " + bad_guys [picker2].name);
+				} else if (Input.GetKeyDown (KeyCode.Space)) {
+					SendMessagey (good_guys [picker].name + " will target " + bad_guys [picker2].name + "!");
+					good_guys [picker].GetComponent<FightBehavior> ().setTarget (bad_guys [picker2]);
+					state = "pick actions";
 				}
+			}
+			return;
+
+		case ("select teammate"):
+			if (Input.GetKeyDown (KeyCode.Backspace)) {
+				state = "pick actions";
+				continuer = false;
+				Debug.Log ("Pick an action for " + good_guys [picker].name + ": A to attack or G to guard!");
+			}
+			if (picker2 == -1) {
+				picker2++;
+				Debug.Log ("Who will " + good_guys[picker].name + " target? Currently targeting: " + good_guys [picker2].name);
+			} else {
+				if (Input.GetKeyDown (KeyCode.W)) {
+					picker2--;
+					if (picker2 < 0) {
+						picker2 = good_guys.Count - 1;
+					}
+					Debug.Log ("Who will " + good_guys[picker].name + " target? Currently targeting: " + good_guys [picker2].name);
+				} else if (Input.GetKeyDown (KeyCode.S)) {
+					picker2++;
+					if (picker2 >= good_guys.Count) {
+						picker2 = 0;
+					}
+					Debug.Log ("Who will " + good_guys[picker].name + " target? Currently targeting: " + good_guys [picker2].name);
+				} else if (Input.GetKeyDown (KeyCode.Space)) {
+					SendMessagey (good_guys [picker].name + " will target " + good_guys [picker2].name  + "!");
+					good_guys [picker].GetComponent<FightBehavior> ().setTarget (good_guys [picker2]);
+					state = "pick actions";
+				}
+			}
+			return;
+
+		case ("commence"):
+			if (await_result) {
+				sendPending ();
+				await_result = false;
+				continuer = true;
+				return;
+			}
+			if (continuer){
+				continuer = false;
+				if (victory) {
+					picker = participants.Count;
+				}
+				picker++;
+			} 
+			if (picker < participants.Count) {
+				while (!participants [picker].activeSelf) {
+					picker++;
+					if (picker >= participants.Count) {
+						return;
+					}
+				}
+				SendMessagey (participants [picker].GetComponent<FightBehavior> ().doAction ());
+				await_result = true;
+			}
+			else {
+				picker = 0;
+				state = "check win";
+				SendMessagey ("The turn has ended. Press space to continue.");
 			}
 			return;
 
@@ -90,10 +198,8 @@ public class BattleManager : MonoBehaviour {
 				Debug.Log ("Congratulations, you have won!");
 				state = "";
 			} else {
-				if (Input.GetKeyDown (KeyCode.Space)) {
-					state = "pick actions";
-					Debug.Log ("Pick an action for " + good_guys [picker].name + ": A to attack or G to guard!");
-				}
+				state = "pick actions";
+				picker = -1;
 			}
 			return;
 
@@ -106,6 +212,7 @@ public class BattleManager : MonoBehaviour {
 		Vector3 positiony = new Vector3 (-2f, 2f, 0f);
 		for (int x = 0; x < good.Count; x++) {
 			GameObject temp = (GameObject)Instantiate (Resources.Load (good [x]), positiony, Quaternion.identity);
+			temp.GetComponent<FightBehavior> ().setAlignment (true);
 			good_guys.Add (temp);
 			participants.Add (temp);
 			positiony += new Vector3 (0f, -1f, 0f);
@@ -113,6 +220,7 @@ public class BattleManager : MonoBehaviour {
 		positiony = new Vector3 (2f, 2f, 0f);
 		for (int x = 0; x < bad.Count; x++) {
 			GameObject temp = (GameObject)Instantiate (Resources.Load (bad [x]), positiony, Quaternion.identity);
+			temp.GetComponent<FightBehavior> ().setAlignment (false);
 			bad_guys.Add (temp);
 			participants.Add (temp);
 			positiony += new Vector3 (0f, -1f, 0f);
@@ -122,8 +230,37 @@ public class BattleManager : MonoBehaviour {
 
 	public void KillEnemies(){
 		for (int x = 0; x < bad_guys.Count; x++) {
-			Destroy (bad_guys [x]);
+			bad_guys [x].SetActive (false);
+			bad_guys.Remove (bad_guys [x]);
 			victory = true;
+		}
+	}
+
+	public void kill(GameObject which){
+		bad_guys.Remove (which);
+		good_guys.Remove (which);
+		which.SetActive (false);
+		if (bad_guys.Count == 0) {
+			victory = true;
+		}
+	}
+
+	public void setPending(string message){
+		pending_message = message;
+	}
+
+	public void sendPending(){
+		if (pending_message != "") {
+			SendMessagey (pending_message);
+			pending_message = "";
+		}
+	}
+
+	public void newTarget(GameObject which, bool good){
+		if (good) {
+			which.GetComponent<FightBehavior> ().setTarget(bad_guys [0]);
+		} else {
+			which.GetComponent<FightBehavior> ().setTarget(good_guys [0]);
 		}
 	}
 }
--- Assets/Scripts/FightBehavior.cs
+++ Assets/Scripts/FightBehavior.cs
@@ -4,29 +4,77 @@ using System.Collections;
 public class FightBehavior : MonoBehaviour {
 
 	string turn_action;
+	GameObject target;
+	BattleManager managey;
+	int health;
+	bool good_guy;
 
 	// Use this for initialization
 	void Start () {
 		turn_action = "AI";
+		health = 3;
+		managey = FindObjectOfType<BattleManager> ();
 	}
 
-	public void setAction(string action){
+	public void setAlignment(bool goodness){
+		good_guy = goodness;
+	}
+
+	public string setAction(string action){
 		turn_action = action;
+		if (action == "attacks") {
+			managey.NeedTargeting ('e');
+			return gameObject.name + " will attack this turn!";
+		} else if (action == "guards") {
+			managey.NeedTargeting ('a');
+			return gameObject.name + " will guard this turn!";
+		} else if (action == "insta-kill") {
+			managey.NeedTargeting ('n');
+			return gameObject.name + " has invoked the win condition!";
+		}
+		else {
+			managey.NeedTargeting ('n');
+			return gameObject.name + " will " + action + " this turn!";
+		}
 	}
 
-	public void doAction(){
-		Debug.Log (gameObject.name + " " + turn_action + "!");
-		if (turn_action == "defeats all the enemies") {
-			FindObjectOfType<BattleManager> ().KillEnemies ();
-		} 
-		else if (turn_action == "AI") {
+	public void setTarget(GameObject tar){
+		target = tar;
+	}
+
+	public string damage (int amount, GameObject attacker){
+		health -= amount;
+		if (health <= 0) {
+			managey.kill (gameObject);
+			return gameObject.name + " has been defeated!";
+		}
+		return attacker.name + " attacks " + gameObject.name;;
+	}
+
+	public string doAction(){
+		if (target != null && !target.activeSelf) {
+			managey.newTarget (gameObject, good_guy);
+		}
+		switch (turn_action) {
+
+		case ("insta-kill"):
+			managey.KillEnemies ();
+			return gameObject.name + " defeats all of the enemies!";
+
+		case ("attacks"):
+			managey.setPending (target.GetComponent<FightBehavior>().damage(3, gameObject));
+			return gameObject.name + " attacks " + target.name;
+
+
+		case ("guards"):
+			return gameObject.name + " guards " + target.name;
+
+		default:
 			if (Random.Range (1, 10) <= 5) {
-				Debug.Log (gameObject.name + " hurls a fireball at you!");
+				return gameObject.name + " hurls a fireball at you!";
 			} else {
-				Debug.Log (gameObject.name + " says something mean to you...");
+				return gameObject.name + " says something mean to you...";
 			}
 		}
 	}
-	
-
 }
