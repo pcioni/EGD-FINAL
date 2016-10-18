c1b640122d3c6f9d40ce82179189df6a26b44c69
sirkevin813 <kortrk@rpi.edu>
2016-10-18 17:32:31 -0400

    Added the ability to arrange characters in Scene view. The collider boxes of the "Group" objects define the angle and spacing of your characters.

---
+++ Assets/FitBackgroundToCamera.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Put this script on the background sprite.
+/// </summary>
+public class FitBackgroundToCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		transform.localScale = Vector3.one;
+
+		float width = GetComponent<SpriteRenderer> ().bounds.size.x;
+		float height = GetComponent<SpriteRenderer> ().bounds.size.y;
+
+		float screenHeight = Camera.main.orthographicSize * 2.0f;
+		float screenWidth = screenHeight / Screen.height * Screen.width;
+
+		transform.localScale = new Vector3(screenWidth / width, screenHeight / height, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/PositionCharactersInBattle.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PositionCharactersInBattle : MonoBehaviour {
+	public enum Arrangment{Diagonal, DiagonalReversed}
+	public Arrangment arrangement;
+	public GameObject[] characters;
+	//^ this is likely just the way we do this while merging everything
+	Bounds b;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 left = Vector3.zero;
+		Vector3 right = Vector3.zero;
+
+		Bounds b = GetComponent<BoxCollider2D> ().bounds;
+
+		switch (arrangement) {
+		case Arrangment.Diagonal:
+			left = b.min;
+			right = b.max;
+			break;
+		case Arrangment.DiagonalReversed:
+			left = new Vector3 (b.min.x, b.max.y, 0);
+			right = new Vector3 (b.max.x, b.min.y, 0);
+			break;
+		}
+
+		Debug.DrawLine (left, right);
+
+		//for start
+		b = GetComponent<BoxCollider2D> ().bounds;
+		for (int x = 0; x < characters.Length; x++) {
+			float percentage = (x + 1) / (characters.Length + 1.0f);
+			characters [x].transform.position = Vector2.Lerp (left, right, percentage); 
+			if (arrangement == Arrangment.Diagonal)
+				characters [x].GetComponent<SpriteRenderer> ().sortingOrder = characters.Length - x;
+			else
+				characters [x].GetComponent<SpriteRenderer> ().sortingOrder = x;
+		}
+	}
+}
