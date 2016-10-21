b03ce9ed3ff9b5865455ab7bb71b07977d0afb7b
sirkevin813 <kortrk@rpi.edu>
2016-10-21 09:42:57 -0400

    First stab at a main menu. Next I'll start building the Steam-like level select and character profile viewing menu.

---
+++ Assets/QuitBehavior.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void QuitGame(){
+		Application.Quit ();
+	}
+}
