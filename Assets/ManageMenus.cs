7a065de7bef36a4755673ea867c6793869e8e189
sirkevin813 <kortrk@rpi.edu>
2016-10-22 00:24:58 -0400

    More progress on the Poof level select screen. We're letting there!

---
+++ Assets/ManageMenus.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ManageMenus : MonoBehaviour {
+	public int current_level;
+	string[] game_names = {"", "1. MMO", "2. Party Game", "3. FPS", "4. Survival", "5. Arena Shooter", "6. MMORPG2" };
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
