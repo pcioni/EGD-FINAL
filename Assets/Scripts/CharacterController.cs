d85de341d33138e44288fc990ee4c4e3871f9fb8
Philip Cioni <philcioni@gmail.com>
2016-10-31 01:02:54 -0400

    Quick rebase commit to fix a stupid git error

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -22,7 +22,7 @@ public class CharacterController : MonoBehaviour {
     }
 
     private void HandleMovement() {
-        //InputManager is currently set to 1/10 of a second to smooth input from +-1 to 0
+        //InputManager is currently set to 1/10 of a second to smooth input from +-1 to 0.
         float dx = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(dx * maxSpeed, rb.velocity.y);
     }
