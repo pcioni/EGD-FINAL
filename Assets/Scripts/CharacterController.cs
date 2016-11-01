1d0ef3583942ffe6bbdbc0927afb48939de3e979
Philip Cioni <philcioni@gmail.com>
2016-11-01 19:49:58 -0400

    Some tooltip stuff. Potential UI redesign. Character controller reconfig.

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -24,7 +24,8 @@ public class CharacterController : MonoBehaviour {
     private void HandleMovement() {
         //InputManager is currently set to 1/10 of a second to smooth input from +-1 to 0.
         float dx = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(dx * maxSpeed, rb.velocity.y);
+        float dy = Input.GetAxis("Vertical");
+        rb.velocity = new Vector2(dx * maxSpeed, dy * maxSpeed);
     }
 
 	void Update () {
