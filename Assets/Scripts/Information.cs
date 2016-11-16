99487426720393bc2e590a4700491eb089f1a593
Daniel Haynes <hayned2@gmail.com>
2016-11-16 16:22:29 -0500

    Minor fixes.

--- Assets/Scripts/Information.cs
+++ Assets/Scripts/Information.cs
@@ -11,6 +11,7 @@ public class Information : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		DontDestroyOnLoad (gameObject);
 		inventory = new Dictionary<string, int> ();
 		defaultInventory ();
 
