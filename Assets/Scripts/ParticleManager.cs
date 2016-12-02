357abf4c1dc35b63788883c628f4f119f7585de6
Daniel Haynes <hayned2@gmail.com>
2016-12-02 16:04:25 -0500

    Particle Manager

---
+++ Assets/Scripts/ParticleManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class ParticleManager {
+
+	public static int doEffect(string attack, FightBehavior user, FightBehavior target){
+
+		switch (attack) {
+			
+		case("fireball"):
+			ParticleBehavior temp = (ParticleBehavior)GameObject.Instantiate (Resources.Load ("Particles/fireball"));
+			temp.moveTo_start = user.transform;
+			temp.moveTo_finish = target.transform;
+			return 0;
+
+		default:
+			return 0;
+		}
+
+	}
+}
